9da2be523310ec8c9e68725de6fe5a423437856c
unknown <kiantpetersen@gmail.com>
2023-10-10 14:35:23 +0200

    adding useState card data

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,21 @@ import './App.css'
 
 function App() {
   const [cardData, setCardData] = useState({
-    name: 'Mark Spencer'
-  })
+    cardholder: 'Mark Spencer',
+    cardNumber: '1111 2222 8888 9999',
+    month: '00',
+    year: '00',
+    cvcNumber: '000'
 
+  })
+  function handleChange(e) {
+    const { name, value } = e.target
+    setCardData((prev) => {
+      return {
+        ...prev, [name]: value
+      }
+    })
+  }
 
 
   return (
@@ -22,37 +34,37 @@ function App() {
               <div className='circle-closed'></div>
               <div className='circle-open'></div>
             </div>
-            <p className='card-number'>1111 2222 8888 9999 </p>
+            <p className='card-number'>{cardData.cardNumber} </p>
             <div className='card-front-info'>
-              <p className='card-name'>Mark Spencer</p>
-              <p className=' card-date'>09/01</p>
+              <p className='card-name'>{cardData.cardholder}</p>
+              <p className=' card-date'>{cardData.month}/{cardData.year}</p>
             </div>
           </div>
           <div className='card-back'>
-            <p className='cvc-number'>741</p>
+            <p className='cvc-number'>{cardData.cvcNumber}</p>
           </div>
         </div>
         <div className='col'>
           <form className='pay-form'>
             <div className='pay-form-container'>
               <label htmlFor='cardholder' className='pay-form-label'>CARDHOLDER NAME</label>
-              <input className='card-input' type='text' name='cardholder' placeholder='e.g. Mark Spencer' />
+              <input onChange={handleChange} value={cardData.name} className='card-input' type='text' name='cardholder' placeholder='e.g. Mark Spencer' />
             </div>
             <div className='pay-form-container'>
               <label htmlFor='cardNumber' className='pay-form-label'>CARD NUMBER</label>
-              <input className='card-input' type='text' name='cardNumber' placeholder='1111 2222 8888 9999' />
+              <input onChange={handleChange} value={cardData.cardNumber} className='card-input' type='text' name='cardNumber' placeholder='1111 2222 8888 9999' />
             </div>
             <div className='card-info-container'>
               <div className='pay-form-container' >
                 <label className='pay-form-label'>EXP. DATE (MM/YY)</label>
                 <div className='date-inputs'>
-                  <input placeholder='MM' className='card-input date-input' />
-                  <input placeholder='YY' className='card-input date-input' />
+                  <input onChange={handleChange} name='month' value={cardData.month} placeholder='MM' className='card-input date-input' />
+                  <input onChange={handleChange} name='year' value={cardData.year} placeholder='YY' className='card-input date-input' />
                 </div>
               </div>
               <div className='pay-form-container' >
                 <label className='pay-form-label'>CVC</label>
-                <input className='card-input cvc-input' />
+                <input onChange={handleChange} value={cardData.cvcNumber} name='cvcNumber' className='card-input cvc-input' />
               </div>
             </div>
 
